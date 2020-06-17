a0b12413cee6e0be6e77c79c0633833182c62e97
Loïc Heinrich <heinrichloic@gmail.com>
2020-06-17 12:50:49 +0200

    4 independent players laid out and ready to use

--- lib/main.dart
+++ lib/main.dart
@@ -38,22 +38,28 @@ class MyApp extends StatelessWidget {
                       ]
                   ),
                 ),
-//                Expanded(
-//                  child: Row(
-//                      children: <Widget>[
-//                        Flexible(
-//                          flex: 1,
-//                          fit: FlexFit.loose,
-//                          child: PlayerCounters(scale: 1,),
-//                        ),
-//                        Flexible(
-//                          flex: 1,
-//                          fit: FlexFit.loose,
-//                          child: PlayerCounters(scale: 1,),
-//                        ),
-//                      ]
-//                  ),
-//                ),
+                Spacer(flex: 1,),
+                Flexible(
+                  flex: 4,
+                  fit: FlexFit.tight,
+                  child: Row(
+                      children: <Widget>[
+                        Spacer(flex: 1,),
+                        Flexible(
+                          flex: 9,
+                          fit: FlexFit.tight,
+                          child: PlayerCounters(),
+                        ),
+                        Spacer(flex: 2,),
+                        Flexible(
+                          flex: 9,
+                          fit: FlexFit.tight,
+                          child: PlayerCounters(),
+                        ),
+                        Spacer(flex: 1,),
+                      ]
+                  ),
+                ),
                 Spacer(flex: 1,),
               ]
           ),
